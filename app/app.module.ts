052e87a47f1189d5343edc81f37f8c9883f6135e
achidu <34405753+achidu@users.noreply.github.com>
2019-03-22 20:02:49 -0400

    Add files via upload

---
+++ app/app.module.ts
@@ -0,0 +1,33 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+import { StocksService } from './services/stocks.service';
+import { FormsModule } from '@angular/forms';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { SummaryComponent } from './components/summary/summary.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { ManageComponent } from './components/manage/manage.component';
+import  {AppRoutes } from './app.routes';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    SummaryComponent,
+    DashboardComponent,
+    ManageComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule,
+    AppRoutes,
+  ],
+  
+  providers: [StocksService],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
